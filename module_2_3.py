028eafb9f54f3378ae14406644e38624c1fefd34
DeathDirk <dirkoflife@gmail.com>
2024-07-18 18:18:37 +0300

    1

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,9 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = len(my_list)
 while i > 0:
-    my_list.pop = n
+    n = my_list.pop(0)
     i = i - 1
     if n > 0:
         print(n)
-    else:
-        continue
+    elif n < 0:
+        break
